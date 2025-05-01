d738dc71bcdeb519fd68575327db815b947d762e
Angelo Yang <38714377+AngeloHYang@users.noreply.github.com>
2025-04-30 23:03:08 -0400

    User Entity

---
+++ backend/src/main/java/com/example/onlineorder/entity/User.java
@@ -0,0 +1,52 @@
+package com.example.onlineorder.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password_hashed;
+    private String email;
+
+    
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword_hashed() {
+        return password_hashed;
+    }
+
+    public void setPassword_hashed(String password_hashed) {
+        this.password_hashed = password_hashed;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
